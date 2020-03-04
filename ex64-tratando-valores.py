f8f942328937f10d07733e5112155ff9bbe8a569
Apolonio Santiago Junior <apolocomputacao@gmail.com>
2020-03-03 22:57:33 -0300

    Add files via upload

---
+++ ex64-tratando-valores.py
@@ -0,0 +1,12 @@
+
+### Digitar 3 valores e depois sair
+'''
+for c in range(0,3):
+    n = int(input("Digite um valor: "))
+print("FIM")
+'''
+### Imprimir de tras para frente
+
+for d in range(1,6,1):
+    print(d)
+print("FIM")
